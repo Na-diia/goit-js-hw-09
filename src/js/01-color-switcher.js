6c8d43cb5a1eba1cfa61be69692549d855b9e449
Na-diia <108739131+Na-diia@users.noreply.github.com>
2022-11-25 04:33:16 +0200

    update task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
 
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null;
+
+const changeColor = () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.body.style.color = getRandomHexColor();
+};
+
+function onBtnStartClick() {
+  timerId = (changeColor, 1000);
+  refs.btnStart.disabled = false;
+};
+
+const onBtnStopClick = () => {
+  clearInterval(timerId);
+  refs.btnStart.disabled = true;
+};
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
